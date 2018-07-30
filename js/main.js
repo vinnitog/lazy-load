edcad3629b9301807bd224f5e6b1c46ccb641289
vinnitog <vinnitog@gmail.com>
2018-07-30 11:05:53 -0300

    subindo base de arquivos

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// document.ready significa que esssa funcao ira rodar apenas apos o documento html todo ser renderizado
+$(document).ready(function(){
+    $(document).on('scroll', function(){
+        
+    });
+});
